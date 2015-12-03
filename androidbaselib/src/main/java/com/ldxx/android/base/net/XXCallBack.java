097ef446c9f749d85f596f23994736de7eafdfed
ldxx <liaodongxiaoxiao@gmail.com>
2015-12-03 13:52:37 +0800

    添加OKHttp的公共方法

---
+++ androidbaselib/src/main/java/com/ldxx/android/base/net/XXCallBack.java
@@ -0,0 +1,25 @@
+package com.ldxx.android.base.net;
+
+import com.squareup.okhttp.Callback;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
+
+import java.io.IOException;
+
+/**
+ * Created by LDXX on 2015/12/3.
+ * company Ltd
+ * liaodongxiaoxiao@gmail.com
+ */
+public class XXCallback implements Callback {
+
+    @Override
+    public void onFailure(Request request, IOException e) {
+
+    }
+
+    @Override
+    public void onResponse(Response response) throws IOException {
+
+    }
+}
